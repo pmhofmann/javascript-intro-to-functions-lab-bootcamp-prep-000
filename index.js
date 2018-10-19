10518006433b3175c2e4d099041c515e838796e5
Patrick Hofmann <pm.hofmann@yahoo.co.uk>
2018-10-19 18:01:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+string = HELLO
 function shout(string) {return string.toUpperCase()}
 function whisper(string) {return string.toLowerCase()}
 function logShout(string) {console.log(string.toUpperCase())}
