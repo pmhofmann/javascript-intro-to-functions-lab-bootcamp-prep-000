1001c8bd7910e83f296f9402c52b534975708d97
Patrick Hofmann <pm.hofmann@yahoo.co.uk>
2018-10-19 18:53:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function logShout(string) {console.log(string.toUpperCase())}
 function logWhisper(string) {console.log(string.toLowerCase())}
 function sayHiToGrandma(string) {
     if (whisper(string) === string) {
-    return "I can\'t hear you!";
+    return "I can't hear you!";
   } else if (shout(string) === string) {
     return "YES INDEED!";
   } else if (string === "I love you, Grandma.") {
